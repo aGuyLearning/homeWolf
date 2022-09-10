6aaca81d710bea347f9ce6df91168e93f0c9cfb4
Carl Buchholz <32228189+aGuyLearning@users.noreply.github.com>
2022-09-10 13:22:14 +0200

    basic config can be loaded and game can be started
    missing game loop, but phase changes

---
+++ homeWolf/__init__.py
@@ -0,0 +1,71 @@
+from queue import Empty
+from socket import socket
+from flask import Flask, request, render_template
+from flask_socketio import SocketIO, send, emit
+import json
+
+from homeWolf.game import Game
+
+socketio = SocketIO()
+game = None
+
+def create_app():
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True,
+                template_folder='templates')
+    app.config.from_mapping(
+        SECRET_KEY='dev',  # change for deployment
+    )
+    @socketio.on('connect')
+    def handle_connect():
+       global game
+       if not game:
+        game = Game()
+    
+
+    @socketio.on('join_game')
+    def join_game(name):
+        if game.players is Empty:
+            game.handle_player_connection(name,request.sid,role='teller')
+        else:
+            game.handle_player_connection(name,request.sid)
+        print(game.players[name])
+    
+
+   
+
+    @socketio.on('get_config')
+    def config():
+        print(request.method)
+        emit('get_config', game.config)
+    
+    @socketio.on('set_config')
+    def set_config(config):
+        game.config = config
+        game.print_config()
+
+    @socketio.on('remove_player')
+    def remove_player(name):
+        game.remove_player(name)
+
+
+    @socketio.on('start_game')
+    def start_game():
+        # check if number of players is reached, excluding the 
+        if game.config['spieler'] != len(game.players.keys()) -1:
+            game.current_phase = 'start'
+            # TODO start game loop
+
+
+
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    socketio.on('vote')
+    def vote(vote):
+        print(vote)
+
+    socketio.init_app(app)
+    return app
---
+++ homeWolf/game.py
@@ -0,0 +1,57 @@
+'''
+Config hat folgende Keys:
+    {
+        spieler: int
+        werwoelfe: int
+        anonymeAbstimmung: boolean
+        maedchen: boolean
+        mystiker: boolean
+        amor: boolean
+        hexe: boolean
+        jaeger: boolean
+        dieb: boolean
+        dorfdepp: boolean
+        seherin: boolean
+    }
+'''
+
+class Game:
+    def __init__(self) -> None:
+        self.config = {
+            'spieler': 7,
+            'werwoelfe': 2,
+            'anonymeAbstimmung': False,
+            'maedchen': True,
+            'mystiker': False,
+            'amor': True,
+            'hexe': True,
+            'jaeger': False,
+            'dieb': False,
+            'dorfdepp': False,
+            'seherin': True
+        }
+        self.current_phase = 'lobby'
+        self.teller = "";
+        self.players = {}
+
+
+    def print_config(self):
+        print(self.config)
+    
+    def player_is_in_game(self,name):
+        return name in self.players.keys()
+    def handle_player_connection(self, name, sid, role='player'):
+        print(self.player_is_in_game(name))
+        if not self.player_is_in_game(name) and self.current_phase == 'lobby':
+            self.players[name] = {
+                    'role': role,
+                    'sid': sid
+                }
+            if role == 'teller':
+                self.teller = name
+        elif self.player_is_in_game(name): 
+            self.players[name]['sid'] = sid
+    
+    def remove_player(self,name):
+        if self.player_is_in_game(name):
+            del self.players[name]
