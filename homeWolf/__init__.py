efa66fd8ad9abbf8c2d1da5006841da84f8b6217
Carl Buchholz <32228189+aGuyLearning@users.noreply.github.com>
2022-09-10 14:17:28 +0200

    removed flask_cors

--- homeWolf/__init__.py
+++ homeWolf/__init__.py
@@ -2,17 +2,15 @@ from queue import Empty
 from socket import socket
 from flask import Flask, request, render_template
 from flask_socketio import SocketIO,  emit
-from flask_cors import CORS
 
 from homeWolf.game import Game
 
-socketio = SocketIO(cors_allowed_origins="*")
+socketio = SocketIO(cors_allowed_origins="127.0.0.1:4200")
 game = None
 
 def create_app():
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True,template_folder='templates')
-    CORS(app)
     app.config.from_mapping(
         SECRET_KEY='dev',  # change for deployment
     )
